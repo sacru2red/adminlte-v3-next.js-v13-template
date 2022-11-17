0374ca03168e637b93fac12f1018637b170ae0f0
sacru2red <sacru2red@gmail.com>
2022-11-17 18:02:15 +0900

    working time

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { defaultLoaders }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: require('styled-jsx/webpack').loader,
+          options: {
+            type: (fileName, options) => options.query.type || 'scoped'
+          }
+        }
+      ]
+    })
+
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: "node_modules/admin-lte/dist", to: "public/admin-lte" },
+        ],
+      }),
+    )
+
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+// import '../styles/globals.css'
+// import 'admin-lte/dist/css/adminlte.min.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import adminLteStyle from 'admin-lte/dist/css/adminlte.min.css?type=global'
+// import adminLte from 'admin-lte/dist/js/adminlte.min.js'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <style jsx global>{adminLteStyle}</style>
+    </React.Fragment>
+  )
 }
